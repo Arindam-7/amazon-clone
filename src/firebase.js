d50e6ecf79acd7a6e48f82db263ef04f740f4a37
Arindam-7 <msaha6368@gmail.com>
2020-09-09 15:39:06 +0530

    added firebase config and cleared the clutter

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDkaB_bkh6F10_4p-ycXrw3fID5fn6i-iQ",
+    authDomain: "clone-2c6b1.firebaseapp.com",
+    databaseURL: "https://clone-2c6b1.firebaseio.com",
+    projectId: "clone-2c6b1",
+    storageBucket: "clone-2c6b1.appspot.com",
+    messagingSenderId: "328736189396",
+    appId: "1:328736189396:web:e50803ffdd5440faeb4441",
+    measurementId: "G-DK7GT4GJGT"
+  };
