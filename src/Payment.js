9f115ed437a6a02e3b5aaf5d924ffd71e971ee0b
Arindam-7 <msaha6368@gmail.com>
2020-09-17 14:00:17 +0530

    added link in header to proceed backwards

--- src/Payment.js
+++ src/Payment.js
@@ -3,6 +3,8 @@ import React from 'react';
 import './Payment.css';
 import { useStateValue } from './StateProvider';
 import CheckoutProduct from './CheckoutProduct';
+import { Link } from 'react-router-dom';
+
 
 
 function Payment() {
@@ -12,6 +14,12 @@ function Payment() {
     return (
         <div className='payment'>
             <div className='payment_container'>
+                <h1>
+                    Checkout (
+                        <Link to='/checkout'>{basket?.length} items</Link>
+                    )
+                </h1>
+
                 <div className='payment_section'>
                     <div className='payment_title'>
                         <h3>Delivery Address</h3>
